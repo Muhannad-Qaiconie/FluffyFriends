79b78b4941980f009b5b69489a0a363797840321
Muhannad <144445326+Muhannad-Qaiconie@users.noreply.github.com>
2023-11-23 09:28:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToServices() {
+    document.getElementById('services').scrollIntoView({ behavior: 'smooth' });
+}
+
+
+Headers
+
+document.getElementById("subscribe-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    alert("Thanks for subscribing!"); 
+});
+
+
